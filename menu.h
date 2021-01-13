736c42ca331171177119bb87a2e8fb7d6b060394
dolev7 <d.dolev7@gmail.com>
2021-01-13 22:20:58 +0200

    with menu

---
+++ menu.h
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Date.h"
+#include "Citizen.h"
+#include "Party.h"
+#include "County.h"
+#include "CountyDelegate.h"
+#include "App.h"
+#include "SimpleApp.h"
+#include "RegularApp.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+using namespace votes;
+
+void startMenu();
